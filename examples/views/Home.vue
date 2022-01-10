92af84c49bea11072a58c43d469c1b4f2a5f7f4a
i7month <lixiaoxu10@163.com>
2022-01-10 10:47:30 +0800

    _透传

--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <lButton @click="send">我是测试的按钮</lButton>
+    <lButton @click="send" size="large">我是测试的按钮</lButton>
     <l-input v-model="value" label="文本" placeholder="请输入用户名"></l-input>
   </div>
 </template>
--- packages/views/lButton/index.vue
+++ packages/views/lButton/index.vue
@@ -1,24 +1,10 @@
 <template>
-  <van-button v-bind="customizedAttrs" :size="size" v-on="$listeners" type="primary"><slot /></van-button>
+  <van-button v-bind="$attrs"  v-on="$listeners" type="primary"><slot /></van-button>
 </template>
 <script>
 export default {
   name: "lButton",
   inheritAttrs: false,
-  props: {
-    size: {
-      type: String,
-      default: "large",
-    },
-  },
-  computed: {
-    customizedAttrs() {
-      return {
-        // 支持传过来的size覆盖默认的size
-        ...this.$attrs,
-      };
-    },
-  }
 };
 </script>
 <style lang="scss" scoped>
